788f70f18d3c0f287481ec6dcbb97ea7cfd78914
Andrii Ferenchuk <ferenchuk.a@gmail.com>
2026-01-03 23:42:31 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { HomeComponent } from "./home/home.component"
 
 @Component({
   standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  template: `
+    <header class="brand-name"><img
+        class="brand-logo"
+        src="/assets/logo.svg"
+        aria-hidden="true" /></header>
+    <main>    
+    
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
+  styleUrls: ["./app.component.css"],
+  imports: [HomeComponent],
 })
 export class AppComponent {
-  title = 'homes';
+  title = "homes"
 }
